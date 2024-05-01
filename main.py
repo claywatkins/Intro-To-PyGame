e03cbd3b2f3d349ec3b1b065cac31e3c5c9f4cb3
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-01 14:05:38 -0700

    created window and ability to close window

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import pygame
 
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+
+while True: 
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    # draw our elements
+    # update everything
+
+    pygame.display.update()
