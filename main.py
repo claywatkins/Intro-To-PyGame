867a55a083059664ba90ef08b5789e1f0a8961c9
Clayton Watkins <watkinsclayton17@yahoo.com>
2024-05-01 14:35:28 -0700

    created clock and set tick rate

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from sys import exit
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
+clock = pygame.time.Clock()
 
 while True: 
     for event in pygame.event.get():
@@ -15,3 +16,4 @@ while True:
     # update everything
 
     pygame.display.update()
+    clock.tick(60)
